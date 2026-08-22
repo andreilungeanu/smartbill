575640fd963bb5557dadb5864b9ae936028f49a5
Andrei Lungeanu <andrei@lungeanu.ro>
2026-08-22 10:35:00 +0300

    feat: annotate the Smartbill facade with endpoint methods

--- src/Facades/Smartbill.php
+++ src/Facades/Smartbill.php
@@ -2,10 +2,25 @@
 
 namespace AndreiLungeanu\Smartbill\Facades;
 
+use AndreiLungeanu\Smartbill\Endpoints\DocumentEndpoint;
+use AndreiLungeanu\Smartbill\Endpoints\EstimatesEndpoint;
+use AndreiLungeanu\Smartbill\Endpoints\InvoicesEndpoint;
+use AndreiLungeanu\Smartbill\Endpoints\PaymentsEndpoint;
+use AndreiLungeanu\Smartbill\Endpoints\SeriesEndpoint;
+use AndreiLungeanu\Smartbill\Endpoints\StocksEndpoint;
+use AndreiLungeanu\Smartbill\Endpoints\TaxesEndpoint;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \AndreiLungeanu\Smartbill\Smartbill
+ *
+ * @method static InvoicesEndpoint invoices()
+ * @method static EstimatesEndpoint estimates()
+ * @method static PaymentsEndpoint payments()
+ * @method static TaxesEndpoint taxes()
+ * @method static SeriesEndpoint series()
+ * @method static StocksEndpoint stocks()
+ * @method static DocumentEndpoint document()
  */
 class Smartbill extends Facade
 {
